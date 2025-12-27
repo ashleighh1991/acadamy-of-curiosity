c295d2992efe288d38b13f7a1ec5aec591db67c9
ashleighh1991 <ashleighhuskinson2@gmail.com>
2025-12-27 07:14:23 +0100

    Hardcode Firebase config for testing

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,21 +3,15 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDCiT_WhSm9wTiOuIniW0jFxO1bROepv3Y",
+  authDomain: "academy-of-curiosity.firebaseapp.com",
+  projectId: "academy-of-curiosity",
+  storageBucket: "academy-of-curiosity.firebasestorage.app",
+  messagingSenderId: "710632620518",
+  appId: "1:710632620518:web:4e0b9b9cd9c0a4306a5627",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
-
-// Initialize Cloud Firestore and get a reference to the service
 export const db = getFirestore(app);
-
-export default app;// Trigger rebuild
+export default app;
